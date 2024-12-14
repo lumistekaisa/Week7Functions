9591c64e4aeee94bd26efbd84ded9e65c6e7e27f
Kaisa Lumiste <lumistekaisa@gmail.com>
2024-12-14 22:17:23 +0200

    week7

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+int a = 5;
+int b = 6;
+
+Add(a, b);
+Add(10, 100);
+Add(56583, 658686);
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(56583, 65);
+
+Substract(a, b);
+Substract(100, 10);
+Substract(56583, 65);
+
+Divide(a, b);
+Divide(100, 10);
+Divide(56583, 65);
+
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+static void Substract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
+
+static void Divide(int x, int y)
+{
+    Console.WriteLine($"{x} / {y} = {x / y}");
+}
---
+++ DisplayArrayElementsFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+string[] pets = { "cats", "dogs", "fish", "hamsters" };
+string[] characters = { "Harry", "Ron" };
+
+PrintArrayElements(fruitBasket);
+PrintArrayElements(pets);
+PrintArrayElements(characters);
+
+static void PrintArrayElements(string[] someArray)
+{
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {someArray[i]}");
+    }
+}
